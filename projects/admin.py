9ab10bc2c2cc9ca9859f35ffa3634803b1d65983
Mohan <mohan.kst.hp@gmail.com>
2017-02-04 13:20:28 +0600

    Added image inline in model admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import Project
+from .models import Project, Image
 
-admin.site.register(Project)
+class ImageInline(admin.StackedInline): #adding the ability to upload images with a Project
+	model = Image
+
+class ProjectAdmin(admin.ModelAdmin): #Admin for handling Projects
+	inlines = [ImageInline,]
+
+admin.site.register(Project, ProjectAdmin)
