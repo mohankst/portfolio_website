3e9c63670a69b6aa8262bfb842d0a1516adf6d32
Mohan <mohan.kst.hp@gmail.com>
2017-02-10 02:43:09 +0600

    added project list and detail view

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Project
 
 def project_list(request):
 	projects = Project.objects.all()
 	return render(request, 'projects/project_list.html', {'projects': projects})
+
+
+def project_detail(request, slug=None):
+	project = get_object_or_404(Project, slug=slug)
+	return render(request, 'projects/project_detail.html', {'project': project})
