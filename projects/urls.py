481eb0abeea0f3057d0fde05c013e2e0431910a3
Mohan <mohan.kst.hp@gmail.com>
2017-02-10 02:44:00 +0600

    added name

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.project_list),
+	url(r'^$', views.project_list, name='list'),
+	url(r'^(?P<slug>[\w-]+)/$', views.project_detail, name='detail'),
 ]
